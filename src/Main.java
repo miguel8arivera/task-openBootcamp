deb8d82032fb2ee1f10f76f483ef5b90b90662f8
Miguel Angel <miguel.angel.8a.rivera@gmail.com>
2022-04-28 11:48:43 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        var result=addition(20,30);
+        System.out.println(result);
+    }
+
+static int addition(int a, int b){
+        return a +b;
+}
+}
+
