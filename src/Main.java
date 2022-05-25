71f2a9b3a4153599047a6b2f9084cc4acc0b462d
Miguel Angel <miguel.angel.8a.rivera@gmail.com>
2022-05-25 07:04:23 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 
+        var name = "kamila";
+        System.out.println(name.length());
+
         var result=addition(20,30);
         System.out.println(result);
+
+        Automovil nuevoAutomovil = new Automovil();
+
+        nuevoAutomovil.aumentarPuertas();
+
+        System.out.println(nuevoAutomovil.numeroPuerta);
+
+
     }
 
 static int addition(int a, int b){
         return a +b;
 }
+}
+
+class Automovil {
+    public int numeroPuerta =3;
+
+    public void aumentarPuertas(){
+        this.numeroPuerta ++;
+    }
 }
 
